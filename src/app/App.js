a7af4d85ece9c43bdf7b93711293addeccb37cc6
Rahul Jadhav <rahul.rj9421@gmail.com>
2020-02-21 23:47:25 +0530

    Rearranged directory structure and added scss structure and added scssRearranged directory structure and added scss

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../css/global/app.scss";
+
+const App = () => {
+  return (
+    <div className="app">
+    This is KL.
+    </div>
+  );
+}
+
+export default App;
